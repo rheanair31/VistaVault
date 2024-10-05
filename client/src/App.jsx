c8ce88f11019551fcab40e9635e90a9aa1b03516
Rhea Nair <rhea31@somaiya.edu>
2024-10-05 16:49:05 +0530

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route}from 'react-router-dom'
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return <h1 className='text-red-500'>APP</h1>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Sign-in" element={<SignIn/>}/>
+    <Route path="/Sign-up" element={<SignUp/>}/>
+    <Route path="/About" element={<About/>}/>
+    <Route path="/Profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>;
 }
