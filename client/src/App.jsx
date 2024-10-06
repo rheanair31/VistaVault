43d3274b9c17fb02b622311e51b28620c486ce9a
Rhea Nair <rhea31@somaiya.edu>
2024-10-06 12:26:26 +0530

    connect to database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,10 @@ import Header from './components/Header';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
-import About from './pages/About';
 import Profile from './pages/Profile';
 import Search from './pages/Search';
+import About from './pages/About';
+
 
 export default function App() {
   return (
@@ -13,10 +14,10 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Sign-in" element={<SignIn />} />
-        <Route path="/Sign-up" element={<SignUp />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} /> 
+        <Route path="/profile" element={<Profile />} />
         <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
