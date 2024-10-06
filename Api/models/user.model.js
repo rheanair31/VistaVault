b3701b9b83f89b2d4826f7418b02fdfb7af4a462
Rhea Nair <rhea31@somaiya.edu>
2024-10-06 14:20:12 +0530

    create sign up api route

--- Api/models/user.model.js
+++ Api/models/user.model.js
@@ -17,5 +17,5 @@ const userSchema=new mongoose.Schema({
 
 },{timestamps:true});
 
-const User=mongoose.model('User',userschema);
+const User=mongoose.model('User',userSchema);
 export default User;
